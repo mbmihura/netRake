e0500736f6c428a4890be962caa71a5a2bc34322
unknown <Martin@Martin-W7.(none)>
2012-05-10 16:25:19 -0300

    Refactorize Main to abstract it and to make it function with any type defined in netRake.commands.

--- netRake/Comando.cs
+++ netRake/Comando.cs
@@ -8,47 +8,47 @@ namespace netRake
 {
      interface IComando
     {
-         void Ejecutar(string args);
+         void Execute(string[] args);
     }
 }
 
-namespace netRake.Comandos
+namespace netRake.Commands
 {
-    class Generate : IComando
+    class generate : IComando
     {
 
-        public Generate()
+        public generate()
         {
             Console.WriteLine("se genero!");
         }
 
-        public void Ejecutar(string args)
+        public void Execute(string[] args)
         {
-            switch (args)
+            switch (args[0])
             {
-                case null:
+                case "view":
                     Console.WriteLine("Working dir: "+ Environment.CurrentDirectory);
                     break;
                 default:
-                    throw new IncorrectArgumentException(this, args);
+                    throw new IncorrectArgumentException(this, args[0]);
             }
         }
     }
 
     class help : IComando
     {
-        public void Ejecutar(string args)
+        public void Execute(string[] args)
         {
-            switch (args)
+            switch (args[0])
             {
                 case null:
                     Console.WriteLine("Available commands:");
 
-                    foreach (string classname in Program.Comandos.Keys)
+                    foreach (string classname in Program.Commands.Keys)
                         Console.WriteLine("  " + classname);
                     break;
                 default:
-                    throw new IncorrectArgumentException(this, args);
+                    throw new IncorrectArgumentException(this, args[0]);
             }
         }
     }
--- netRake/Program.cs
+++ netRake/Program.cs
@@ -5,34 +5,39 @@ using System.Linq.Expressions;
 using System.Text;
 using System.IO;
 using System.Reflection;
+using netRake.Commands;
 
 namespace netRake
 {
     class Program
     {
-        static List<Control> _controles = new List<Control>();
-        static Dictionary<string, Type> _comandos = new Dictionary<string, Type>();
+        static List<Control> _controls = new List<Control>();
+        static Dictionary<string, Type> _comands = new Dictionary<string, Type>();
 
-        static public Dictionary<string, Type> Comandos { get { return _comandos;} }
+        static public Dictionary<string, Type> Commands { get { return _comands;} }
 
         static void Main(string[] args)
         {
-
+            //Creates dictionary with commands defined through reflection of classes in command's namespace (netRake.Commands)
             foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
             {
-               if (type.Namespace == "netRake.Comandos")
-                    _comandos.Add(type.Name, type);
+               if (type.Namespace == typeof(help).Namespace)
+                    _comands.Add(type.Name, type);
             }
 
-            args = new string[] { "Generate", null };
+            //HACK: handy, automatic way to introduce a command during development. Eliminate in production.
+            args = new string[] { "generate", "view" };
 
-            Type t = _comandos[args[0]];
-            ConstructorInfo comando = t.GetConstructor(new Type[] {});
-            string[] arg = args.Skip(1).ToArray();
-            object var = comando.Invoke(null);
+            //Extracts Type from the first argument, 
+            Type commandType = _comands[args[0]];
 
-            netRake.Comandos.Generate g = (netRake.Comandos.Generate) var;
-            g.Ejecutar(args[1]);
+            //Creates an objects from that type
+            ConstructorInfo commandConstructorInfo = commandType.GetConstructor(new Type[] { });
+            object commandObj = commandConstructorInfo.Invoke(null);
+            dynamic command = Convert.ChangeType(commandObj, commandType);
+
+            //Execute using the rest of the arguments
+            command.Execute(args.Skip(1).ToArray());
         }
     }
 }
